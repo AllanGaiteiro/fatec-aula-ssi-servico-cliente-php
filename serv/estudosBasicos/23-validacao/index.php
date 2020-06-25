b6a486071f54e44da39be0cb68c954bfe2c7ce91
Allan Gaiteiro <64567248+AllanGaiteiro@users.noreply.github.com>
2020-06-24 21:32:47 -0300

    aula php

--- serv/estudosBasicos/23-validacao/index.php
+++ serv/estudosBasicos/23-validacao/index.php
@@ -3,6 +3,11 @@
 
 <head>
     <title>Formulario</title>
+    <style>
+        input {
+            display: block;
+        }
+    </style>
 </head>
 
 <body>
@@ -15,12 +20,39 @@
     //FILTER_VALIDATE_
     //FILTER_VALIDATE_INT
     ?>
-    <form action="dados .php" method="GET">
-        Nome: <input type="text" name="nome" id="">
-        Email: <input type="email" name="email" id="">
-        <button type="submit">Enviar</button><br>
+
+
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+        Idade: <input type="text" name="idade" id="">
+        Email: <input type="text" name="email" id="">
+        Peso: <input type="text" name="peso" id="">
+        IP: <input type="text" name="ip" id="">
+        url: <input type="text" name="url" id="">
+
+        <button type="submit" name="enviar-form">Enviar</button><br>
     </form>
-    <a href="dadosGET.php?idade=23&estado=solteiro">Enviar dados</a>
+        <?php
+    // isset // existe
+    if(isset($_POST['enviar-form'])):// se existir o paramentoro na super global post
+        //echo "Enviou <br>";
+        var_dump($_POST);// ve como strin os posts
+        $erros = array();
+        //$idade = $_POST['idade'];
+        //$email = $_POST['email'];
+        if(!$idade = filter_input(INPUT_POST,'idade', FILTER_VALIDATE_INT)):// esquisito 
+            $erros[] = "idade precisa ser um inteiro";
+        endif;
+
+        if(!empty($erros)):// se a variavel esta vazia
+            foreach($erros as $indice => $erro):
+                echo "<li>ERROR $indice: $erro <li>";
+            endforeach;
+        else:
+            echo "Parabéns, seus dados estão corretos";
+        endif;
+        print_r((!empty($erros))? $erros : null);// meu jeito
+    endif; 
+    ?>
 </body>
 
 </html>
